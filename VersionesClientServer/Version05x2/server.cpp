530f43520a2ba25e49912e1a89d9960627a72fc1
RodATS <77297145+RodATS@users.noreply.github.com>
2023-04-23 18:22:51 -0500

    Update server.cpp

--- VersionesClientServer/Version05x2/server.cpp
+++ VersionesClientServer/Version05x2/server.cpp
@@ -19,7 +19,8 @@ _4_|_5_|_6_
 _7_|_8_|_9_
 */
 
-int Tablero = [[0,0,0],[0,0,0],[0,0,0]];
+int Tablero[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+string fichas[3] = {"-","O","X"};
 
 void ReiniciarTablero(){
 	for(int i =0; i < 3; i++){
@@ -29,15 +30,129 @@ void ReiniciarTablero(){
 	}
 }
 
+
 //Jugador1 = 1,  Jugador2 = 2
 int VerficarGanador(){
 	int ganador = 0; //0: indica que aún no hay ganador
 	//gana jugador 1
-	if ( Tablero[0][0] == 1 && Tablero[0][1] == 1 && Tablero[0][2] == 1 ){
-		
+  //modo horizontal
+	if ( (Tablero[0][0] == 1 && Tablero[0][1] == 1 && Tablero[0][2] == 1 ) || 
+     ( Tablero[1][0] == 1 && Tablero[1][1] == 1 && Tablero[1][2] == 1 ) ||
+     ( Tablero[2][0] == 1 && Tablero[2][1] == 1 && Tablero[2][2] == 1 ) ||
+  //vertical
+    ( Tablero[0][0] == 1 && Tablero[1][0] == 1 && Tablero[1][0] == 1 ) ||
+    ( Tablero[0][1] == 1 && Tablero[1][1] == 1 && Tablero[2][1] == 1 ) ||
+    ( Tablero[0][2] == 1 && Tablero[1][2] == 1 && Tablero[2][2] == 1 ) ||
+
+    //diagonal
+    ( Tablero[0][0] == 1 && Tablero[1][1] == 1 && Tablero[2][2] == 1 ) ||
+    ( Tablero[0][2] == 1 && Tablero[1][1] == 1 && Tablero[2][0] == 1 )
+    )
+  {
+		ganador = 1;
 	}
-	
+
+  else{
+      if ( (Tablero[0][0] == 2 && Tablero[0][1] == 2 && Tablero[0][2] == 2 ) || 
+       ( Tablero[1][0] == 2 && Tablero[1][1] == 2 && Tablero[1][2] == 2 ) ||
+       ( Tablero[2][0] == 2 && Tablero[2][1] == 2 && Tablero[2][2] == 2 ) ||
+    //vertical
+      ( Tablero[0][0] == 2 && Tablero[1][0] == 2 && Tablero[1][0] == 2 ) ||
+      ( Tablero[0][1] == 2 && Tablero[1][1] == 2 && Tablero[2][1] == 2 ) ||
+      ( Tablero[0][2] == 2 && Tablero[1][2] == 2 && Tablero[2][2] == 2 ) ||
+  
+      //diagonal
+      ( Tablero[0][0] == 2 && Tablero[1][1] == 2 && Tablero[2][2] == 2 ) ||
+      ( Tablero[0][2] == 2 && Tablero[1][1] == 2 && Tablero[2][0] == 2 )
+      )
+    {
+  		ganador = 2;
+  	}
+  }
+
+  return ganador;
 }
+
+bool TableroCompleto(){
+  for(int i =0; i < 3; i++){
+		for(int j =0;j<3; j++){
+			if(Tablero[i][j] == 0){
+        return false;
+      }
+		}
+	}
+
+  //Si el tablero ya esta completo
+  return true;
+}
+
+
+void ColocarFicha(int pos, int jugador){
+  /*
+  3 en raya
+  _1_|_2_|_3_
+  _4_|_5_|_6_
+  _7_|_8_|_9_
+*/
+  switch (pos){
+    case 1:{
+        Tablero[0][0] = jugador;
+        break;
+    }
+
+    case 2:{
+        Tablero[0][1] = jugador;
+        break;
+    }
+
+    case 3:{
+        Tablero[0][2] = jugador;
+        break;
+    }
+
+    case 4:{
+        Tablero[1][0] = jugador;
+        break;
+    }
+
+    case 5:{
+        Tablero[1][1] = jugador;
+        break;
+    }
+
+    case 6:{
+        Tablero[1][2] = jugador;
+        break;
+    }
+
+    case 7:{
+        Tablero[2][0] = jugador;
+        break;
+    }
+
+    case 8:{
+        Tablero[2][1] = jugador;
+        break;
+    }
+
+    case 9:{
+        Tablero[2][2] = jugador;
+        break;
+    }
+  }
+}
+
+
+void PrintTablero(){
+  for(int i =0; i < 3; i++){
+		for(int j =0;j<3; j++){
+			cout<<fichas[Tablero[i][j]]<<" | ";
+		}
+    cout<<"\n---------"<<endl;
+	}
+}
+
+//------------------------------------------------------------
 
 
 
@@ -245,8 +360,8 @@ void thread_read(int socketC)
 			cout << "Game Accepted" << endl;
 			
 			//Se agregan los jugadores al map:  map<string,int> jugadores;
-                	jugadores.insert({sockets[socketC], socketC});
-			jugadores.insert({cli,rec});
+                	jugadores.insert({sockets[socketC], 1});
+			jugadores.insert({cli,2});
 			
                 	break;
                 }
@@ -286,7 +401,64 @@ void thread_read(int socketC)
                 	break;
                 }
 			    
+		case 'W': { //Movimientos 3 en raya
 		
+			//Tamaño del mensaje que se recibio
+			n = read(socketC, buffer, 4);
+			buffer[4] = '\0';
+
+			//se lee el movimiento
+			int size_m = atoi(buffer);
+			char csize_m[10000];
+			sprintf(csize_m, "%04d", size_m);
+			n = read(socketC, buffer, size_m);
+			buffer[size_m] = '\0';
+
+			char *msg = (char*)malloc(sizeof(char) * size_m);
+			sprintf(msg, "%s", buffer);
+			
+			//ficha pos movimiento
+			int move = atoi(buffer);
+
+
+			//tamaño del nombre del usuario
+			n = read(socketC, buffer, 4);
+			buffer[4] = '\0';
+
+			//nombre del usuario
+			int userBuffSize = atoi(buffer);
+			n = read(socketC, buffer, userBuffSize);
+			buffer[userBuffSize] = '\0';
+
+			char *cli = (char*)malloc(sizeof(char) * userBuffSize);
+			sprintf(cli, "%s", buffer);
+
+			//el socket del cliente a quien se le mandara el mensaje
+			int rec = users[buffer];
+
+			char user2[10000];
+			sprintf(user2, "%s", sockets[socketC].c_str());
+			char cszuser2[5];
+			int szuser2 = strlen(user2);
+			sprintf(cszuser2, "%04d", szuser2);
+
+			cout << "Jugador: " << sockets[socketC] << " coloco en la posicion " << msg << endl;
+
+			ColocarFicha(move,jugadores[sockets[socketC]]);
+			
+			write(rec, "W", strlen("W"));
+			//tamaño del mensaje
+			write(rec, csize_m, strlen(csize_m));
+			//mensaje
+			write(rec, msg, strlen(msg));
+			//tamaño del nombre del usuario que mando el mensaje
+			write(rec, cszuser2, strlen(cszuser2));
+			//nombre usuario
+			write(rec, user2, strlen(user2));
+
+			cout << "Message sent." << endl;
+			break;
+                }
         
                 
                 
