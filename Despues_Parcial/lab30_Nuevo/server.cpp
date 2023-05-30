b23f484f1f7e6a7750e929b11358b1e1a178d642
RodATS <77297145+RodATS@users.noreply.github.com>
2023-05-30 18:55:41 -0500

    Update server.cpp

--- Despues_Parcial/lab30_Nuevo/server.cpp
+++ Despues_Parcial/lab30_Nuevo/server.cpp
@@ -48,142 +48,147 @@ int main(void){
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 	if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) {
-	fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
-	exit(1);
+		fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
+		exit(1);
 	}
+	
 	for(p = ai; p != NULL; p = p->ai_next) {
 		listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
 		if (listener < 0) {
-		continue;
+			continue;
 		}
 		// lose the pesky "address already in use" error message
 		setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 		if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
-		close(listener);
-		continue;
+			close(listener);
+			continue;
 		}
 		break;
 	}
+	
 	// if we got here, it means we didn't get bound
 	if (p == NULL) {
-	fprintf(stderr, "selectserver: failed to bind\n");
-	exit(2);
+		fprintf(stderr, "selectserver: failed to bind\n");
+		exit(2);
 	}
 	freeaddrinfo(ai); // all done with this
+	
 	// listen
 	if (listen(listener, 10) == -1) {
-	perror("listen");
-	exit(3);
+		perror("listen");
+		exit(3);
 	}
+	
+	
 	// add the listener to the master set
 	FD_SET(listener, &master);
 	// keep track of the biggest file descriptor
 	fdmax = listener; // so far, it's this one
 	// main loop
 	for(;;) {
-	read_fds = master; // copy it
-	if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
-	perror("select");
-	exit(4);
-	}
-	// run through the existing connections looking for data to read
-	for(i = 0; i <= fdmax; i++) {
-	if (FD_ISSET(i, &read_fds)) { // we got one!!
-	if (i == listener) {
-	// handle new connections
-	addrlen = sizeof remoteaddr;
-	newfd = accept(listener,
-	(struct sockaddr *)&remoteaddr,
-	&addrlen);
-	if (newfd == -1) {
-	perror("accept");
-	} else {
-	FD_SET(newfd, &master); // add to master set
-	if (newfd > fdmax) { // keep track of the max
-	fdmax = newfd;
-	}
-	printf("selectserver: new connection from %s on "
-	"socket %d\n",
-	inet_ntop(remoteaddr.ss_family,
-	get_in_addr((struct sockaddr*)&remoteaddr),
-	remoteIP, INET6_ADDRSTRLEN),
-	newfd);
-	}
-	} else {
-	// handle data from a client
-	
-	if ((nbytes = recv(i, buf, 9 , 0)) <= 0) {
-		// got error or connection closed by client
-		if (nbytes == 0) {
-		// connection closed
-		printf("selectserver: socket %d hung up\n", i);
-		} 
-		else {
-		perror("recv");
+		read_fds = master; // copy it
+		if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+			perror("select");
+			exit(4);
 		}
-		close(i); // bye!
-		FD_CLR(i, &master); // remove from master set
-	} 
-	else {
-	// we got some data from a client
-		//nbytes = recv(i, buf, 9 , 0);
-		int indice = 0;
-		
-		
-		buf[9] = '\0';
-		arr_response[indice] = "";
-		arr_response[indice]  +=to_string(count);count++;
-		
-		arr_response[indice]  +=":";
-		arr_response[indice]  += buf;
-		arr_response[indice]  +=":";
-		nbytes = recv(i, buf, 1 , 0);
-		buf[1] = '\0';
-		
-		nbytes = recv(i, buf, 7 , 0);
-		buf[7] = '\0';
-		arr_response[indice]  += buf;
-		arr_response[indice]  +=":";
-		
-		int tam = atoi(buf); //cout<<tam<<endl;
-		nbytes = recv(i, buf, 1 , 0);
-		buf[1] = '\0';
-		
-		nbytes = recv(i, buf, tam , 0);
-		buf[tam] = '\0';
-		arr_response[indice] +=buf;
-		arr_response[indice]  +=":";
-		
-		nbytes = recv(i, buf, 1 , 0);
-		buf[1] = '\0';
-		nbytes = recv(i, buf, 10 , 0);
-		buf[10] = '\0';
-		arr_response[indice] +=buf;
-		arr_response[indice]  +="\n";
-		
-		nbytes = recv(i, buf, 1 , 0);
-		buf[1] = '\0';
-		
-		
-		for(j = 0; j <= fdmax; j++) {
-		// send to everyone!
-		
-			if (FD_ISSET(j, &master)) {
-				// except the listener and ourselves
-				if (j != listener && j != i) {
-				if (send(j, arr_response[indice].c_str(),  strlen(arr_response[indice].c_str()), 0) == -1) {
-				perror("send");
+		// run through the existing connections looking for data to read
+		for(i = 0; i <= fdmax; i++) {
+			if (FD_ISSET(i, &read_fds)) { // we got one!!
+			if (i == listener) {
+			
+				// handle new connections
+				addrlen = sizeof remoteaddr;
+				newfd = accept(listener,(struct sockaddr *)&remoteaddr,&addrlen);
+				
+				if (newfd == -1) {
+					error("accept");
+				} else {
+					FD_SET(newfd, &master); // add to master set
+					if (newfd > fdmax) { // keep track of the max
+						fdmax = newfd;
+					}
+					printf("selectserver: new connection from %s on "
+					"socket %d\n",
+					inet_ntop(remoteaddr.ss_family,
+					get_in_addr((struct sockaddr*)&remoteaddr),
+					remoteIP, INET6_ADDRSTRLEN),
+					newfd);
 				}
+			} else {
+			// handle data from a client
+			
+			if ((nbytes = recv(i, buf, 9 , 0)) <= 0) {
+				// got error or connection closed by client
+				if (nbytes == 0) {
+				// connection closed
+				printf("selectserver: socket %d hung up\n", i);
+				} 
+				else {
+				perror("recv");
 				}
+				close(i); // bye!
+				FD_CLR(i, &master); // remove from master set
+			} 
+			else {
+			// we got some data from a client
+				//nbytes = recv(i, buf, 9 , 0);
+				int indice = 0;
+				
+				
+				buf[9] = '\0';
+				arr_response[indice] = "";
+				arr_response[indice]  +=to_string(count);count++;
+				
+				arr_response[indice]  +=":";
+				arr_response[indice]  += buf;
+				arr_response[indice]  +=":";
+				nbytes = recv(i, buf, 1 , 0);
+				buf[1] = '\0';
+				
+				nbytes = recv(i, buf, 7 , 0);
+				buf[7] = '\0';
+				arr_response[indice]  += buf;
+				arr_response[indice]  +=":";
+				
+				int tam = atoi(buf); //cout<<tam<<endl;
+				nbytes = recv(i, buf, 1 , 0);
+				buf[1] = '\0';
+				
+				nbytes = recv(i, buf, tam , 0);
+				buf[tam] = '\0';
+				arr_response[indice] +=buf;
+				arr_response[indice]  +=":";
+				
+				nbytes = recv(i, buf, 1 , 0);
+				buf[1] = '\0';
+				nbytes = recv(i, buf, 10 , 0);
+				buf[10] = '\0';
+				arr_response[indice] +=buf;
+				arr_response[indice]  +="\n";
+				
+				nbytes = recv(i, buf, 1 , 0);
+				buf[1] = '\0';
+				
+				
+				for(j = 0; j <= fdmax; j++) {
+				// send to everyone!
+				
+					if (FD_ISSET(j, &master)) {
+						// except the listener and ourselves
+						if (j != listener && j != i) {
+						if (send(j, arr_response[indice].c_str(),  strlen(arr_response[indice].c_str()), 0) == -1) {
+						perror("send");
+						}
+						}
+					}
+				}
+				
+				
+				
 			}
-		}
-		
-		
-		
-	}
-	} // END handle data from client
-	} // END got new incoming connection
-	} // END looping through file descriptors
+			} // END handle data from client
+			} // END got new incoming connection
+		} // END looping through file descriptors
 	} // END for(;;)--and you thought it would never end!
 	return 0;
 }
